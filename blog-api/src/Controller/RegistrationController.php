cd9eb3ba52e084af3d0eb6e7301380b5d1517c77
Andrii Irkha <andriiirkha@Andrii-MacBook-Pro.local>
2024-05-10 09:03:54 +0200

    resrt-password

--- blog-api/src/Controller/RegistrationController.php
+++ blog-api/src/Controller/RegistrationController.php
@@ -143,10 +143,12 @@ class RegistrationController extends AbstractController
 
         $url = $this->generateUrl('app-reset-password', ['reset_token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
 
+        $newUrl = str_replace('127.0.0.1:8001', 'symfony-blog.fromargo.com/symfony-blog-api/blog-api/public', $url );
+
         $message = (new Email())
             ->from('formydjangoblog@gmail.com')
             ->to($user->getEmail())
-            ->html("<p>Hello,</p><p>Click <a href=\"$url\">here</a> to reset your password.</p>");
+            ->html("<p>Hello,</p><p>Click <a href=\"$newUrl\">here</a> to reset your password.</p>");
 
         $mailer->send($message);
 
